e3361ff29b473f1f1c48dea605e20323144ee9d7
affan-ch <muhammadaffanmaqsood@gmail.com>
2025-06-04 23:28:39 +0500

    Initial Project

---
+++ Cyber Vault/Cyber_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cyber_VaultApp.swift
+//  Cyber Vault
+//
+//  Created by Affan Ali on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cyber_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
